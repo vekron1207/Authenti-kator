8dd23c697648a44796f397f0b1c6412aeff95eb7
Varun <varun.kashyap1207@gmail.com>
2022-12-26 20:27:38 +0530

    Working bareBones model

--- forms/dbconnection.php
+++ forms/dbconnection.php
@@ -3,27 +3,55 @@
 // Connect to the database
 $host        = "host = localhost";
 $port        = "port = 5432";
-$dbname      = "dbname = Auth_Db";
+$dbname      = "dbname = Auth_DB";
 $credentials = "user = postgres password=0000";
 
 $db = pg_connect( "$host $port $dbname $credentials"  );
-if(!$db) {
-   die("Error : Unable to open database\n");
+
+// Check the connection status
+if (pg_connection_status($db) === PGSQL_CONNECTION_OK) {
+  echo "Connection to the database was successful!\n";
+} else {
+  echo "An error occurred while trying to connect to the database:\n";
+  echo pg_last_error($db) . "\n";
 }
 
-// Get the serial number from the form submission
-$serial_number = $_POST['serial_number'];
+// Get the Serial_No from the form submission
+$serial_number = $_GET['Serial_No'];
 
-// Check if the serial number exists in the database
-$query = "SELECT * FROM Nike_DB WHERE Serial_No='$serial_number'";
+// Check if the Serial_No exists in the database
+$query = "SELECT * FROM public.\"Nike_DB\" WHERE \"Nike_DB\".\"Serial_No\"='$serial_number'";
 $result = pg_query($db, $query);
 
-if (pg_num_rows($result) > 0) {
-   // Serial number found in the database
-   echo "Authentic Product! Congrats";
+if (!$result) {
+  echo "An error occurred while executing the query: " . pg_last_error($db);
+  exit;
+}
+
+$row = pg_fetch_assoc($result);
+
+if (!empty($row)) {
+   // Serial_No found in the database
+   if ($row['is_Authenticated'] == 'true') {
+      // is_Authenticated is already set to true
+      echo "Please contact seller";
+   } else {
+      // is_Authenticated is not set to true, update it
+      echo "Authentic Product! Congrats";
+
+      // Update the "is_Authenticated" column to "true" where the Serial_No is equal to the provided serial number
+      $query = "UPDATE public.\"Nike_DB\" SET \"is_Authenticated\" = 'true' WHERE \"Nike_DB\".\"Serial_No\"='$serial_number'";
+
+      $result = pg_query($db, $query);
+
+      if (!$result) {
+         echo "An error occurred while executing the query: " . pg_last_error($db);
+         exit;
+      }
+   }
 } else {
-   // Serial number not found in the database
-   echo "Invalid serial number";
+   // Serial_No not found in the database
+   echo "Invalid Serial_No";
 }
 
 pg_close($db);
